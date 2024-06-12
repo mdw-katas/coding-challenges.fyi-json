6ea65d73f75f9a58f4587ff94c623417ce153470
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-06-12 00:45:31 -0600

    Feed stdin to lexer and inspect results.

--- cmd/valijson/main.go
+++ cmd/valijson/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/mdwhatcott/coding-challenges.fyi-json/lib/lexing"
 )
 
 var Version = "dev"
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.Ltime)
 	flags := flag.NewFlagSet(fmt.Sprintf("%s @ %s", filepath.Base(os.Args[0]), Version), flag.ExitOnError)
 	flags.Usage = func() {
 		_, _ = fmt.Fprintf(flags.Output(), "Usage of %s:\n", flags.Name())
@@ -19,5 +24,18 @@ func main() {
 	}
 	_ = flags.Parse(os.Args[1:])
 
-	fmt.Println("Hello, world!")
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	byteCount := 0
+	tokenCount := 0
+	for token := range lexing.Lex(input) {
+		if token.Type == lexing.TokenIllegal {
+			log.Fatalf("Illegal token at index %d: %s", byteCount, token.Value)
+		}
+		tokenCount++
+		byteCount += len(token.Value)
+	}
+	log.Printf("JSON document with %d bytes and %d tokens validated successfully.", byteCount, tokenCount)
 }
